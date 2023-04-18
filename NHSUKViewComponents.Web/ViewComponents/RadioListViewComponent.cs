055fd4d2364d07c18f3f6ce01c2d249bf32f6716
OluwatobiAwe <oluwatobi.awe@hee.nhs.uk>
2023-04-18 12:38:50 +0100

    TD-1382 : Added Radio button not dependent on custom DLS class

---
+++ NHSUKViewComponents.Web/ViewComponents/RadioListViewComponent.cs
@@ -0,0 +1,48 @@
+﻿namespace NHSUKViewComponents.Web.ViewComponents
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+    using NHSUKViewComponents.Web.ViewModels;
+
+    public class RadioListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(
+            string aspFor,
+            string label,
+            IEnumerable<RadiosItemViewModel> radios,
+            bool populateWithCurrentValues,
+            string hintText,
+            bool required
+        )
+        {
+            var radiosList = radios.Select(
+                r => new RadiosItemViewModel(
+                    r.Value,
+                    r.Label,
+                    IsSelectedRadio(aspFor, r, populateWithCurrentValues),
+                    r.HintText
+                )
+            );
+
+            var viewModel = new RadiosViewModel(
+                aspFor,
+                label,
+                string.IsNullOrEmpty(hintText) ? null : hintText,
+                radiosList,
+                required
+            );
+
+            return View(viewModel);
+        }
+
+        private bool IsSelectedRadio(string aspFor, RadiosItemViewModel radioItem, bool populateWithCurrentValue)
+        {
+            var model = ViewData.Model;
+            var property = model.GetType().GetProperty(aspFor);
+            var value = property?.GetValue(model);
+            return populateWithCurrentValue && value.Equals(radioItem.Value);
+        }
+    }
+}
